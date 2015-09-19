2d7ede25f06656315b40a928100c31da22c9c860
Dan Nixon <dan@dan-nixon.com>
2015-09-19 09:04:16 +0100

    FIx header guard

--- NextionText.h
+++ NextionText.h
@@ -1,5 +1,5 @@
-#ifndef __NEONEXTION_NEXTIONBUTTON
-#define __NEONEXTION_NEXTIONBUTTON
+#ifndef __NEONEXTION_NEXTIONTEXT
+#define __NEONEXTION_NEXTIONTEXT
 
 #include <Arduino.h>
 #include "Nextion.h"
