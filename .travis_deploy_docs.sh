72e5bb9e4e39ec39b021f48c85153d5c9a443b13
Dan Nixon <dan@dan-nixon.com>
2016-04-08 09:03:49 +0100

    Have Travid CI deploy docs to GH pages

---
+++ .travis_deploy_docs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ];
+then
+  echo Not publishing documentation.
+  exit 0;
+fi
+
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
+cd docs/html
+git init
+
+git add .
+git commit -m "Deploy Doxygen documentation"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
